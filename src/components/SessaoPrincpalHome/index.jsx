b9e32a44a0318a030ebae17bab07932caafc2fdf
barbarabrito <barbaramabrito@gmail.com>
2022-02-06 22:58:50 -0300

    small changes in the newsletter

--- src/components/SessaoPrincpalHome/index.jsx
+++ src/components/SessaoPrincpalHome/index.jsx
@@ -3,6 +3,27 @@ import './style.css';
 function SessaoPrincipalHome() {
     return (
         <div className="container">
+            <div className="container newsletter">
+                <h2>Quer saber sobre novas viagens e promoções de pacotes?</h2>
+                <h6>Assine nossa newsletter e nunca perca nenhuma novidade!</h6>
+                <br />
+                <form className="row row-cols-lg-auto g-2 align-items-center newsletter-form">
+                    <div className="col-12">
+                        <label className="visually-hidden" htmlFor="inlineFormInputGroupUsername">Username</label>
+                        <div className="input-group">
+                            <div className="input-group-text"><i className="fas fa-envelope"></i></div>
+                            <input type="email" className="form-control" id="inlineFormInputGroupUsername"
+                                placeholder="email@exemplo.com" />
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <input type="text" className="form-control" placeholder="nome" aria-label="nome" />
+                    </div>
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-warning" id="btn-newsletter">Inscrever</button>
+                    </div>
+                </form>
+            </div>
             <div className="container pq-section" id="pq">
                 <div className="row pq-section-row">
                     <div className="col-md-4 col-sm-6" id="melhores-precos">
@@ -30,27 +51,6 @@ function SessaoPrincipalHome() {
                         <p>Temos o melhor suporte ao cliente<br/> que você já viu</p>
                     </div>
                 </div>
-            </div>
-            <div className="container newsletter">
-                <h2>Quer saber sobre novas viagens e promoções de pacotes?</h2>
-                <h6>Assine nossa newsletter e nunca perca nenhuma novidade!</h6>
-                <br />
-                <form className="row row-cols-lg-auto g-2 align-items-center newsletter-form">
-                    <div className="col-12">
-                        <label className="visually-hidden" htmlFor="inlineFormInputGroupUsername">Username</label>
-                        <div className="input-group">
-                            <div className="input-group-text"><i className="fas fa-envelope"></i></div>
-                            <input type="email" className="form-control" id="inlineFormInputGroupUsername"
-                                placeholder="email@exemplo.com" />
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <input type="text" className="form-control" placeholder="nome" aria-label="nome" />
-                    </div>
-                    <div className="col-12">
-                        <button type="submit" className="btn btn-warning" id="btn-newsletter">Inscrever</button>
-                    </div>
-                </form>
             </div>
         </div>
     );

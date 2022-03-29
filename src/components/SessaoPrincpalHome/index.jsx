534246d1e64573c1f36e9b1a7d9ddd6db5434c7f
barbarabrito <barbaramabrito@gmail.com>
2022-03-29 17:03:46 -0300

    fix- newsletter

--- src/components/SessaoPrincpalHome/index.jsx
+++ src/components/SessaoPrincpalHome/index.jsx
@@ -7,20 +7,17 @@ function SessaoPrincipalHome() {
                 <h2>Quer saber sobre novas viagens e promoções de pacotes?</h2>
                 <h6>Assine nossa newsletter e nunca perca nenhuma novidade!</h6>
                 <br />
-                <form className="row row-cols-lg-auto g-2 align-items-center newsletter-form">
+                <form className="row  g-2 newsletter-form">
                     <div className="col-12">
-                        <label className="visually-hidden" htmlFor="inlineFormInputGroupUsername">Username</label>
+
                         <div className="input-group">
                             <div className="input-group-text"><i className="fas fa-envelope"></i></div>
                             <input type="email" className="form-control" id="inlineFormInputGroupUsername"
                                 placeholder="email@exemplo.com" />
+                           
+                                <button type="submit" className="btn btn-warning" id="btn-newsletter">Inscrever</button>
+                           
                         </div>
-                    </div>
-                    <div className="col-12">
-                        <input type="text" className="form-control" placeholder="nome" aria-label="nome" />
-                    </div>
-                    <div className="col-12">
-                        <button type="submit" className="btn btn-warning" id="btn-newsletter">Inscrever</button>
                     </div>
                 </form>
             </div>
@@ -32,7 +29,7 @@ function SessaoPrincipalHome() {
                         <i className="fa fa-hand-holding-heart"></i>
                         <br />
                         <br />
-                        <p>Porque temos os melhores preços <br/>e o melhor serviço</p>
+                        <p>Porque temos os melhores preços <br />e o melhor serviço</p>
                     </div>
                     <div className="col-md-4 col-sm-6" id="rapido-e-facil">
                         <h4>Rápido e fácil</h4>
@@ -40,7 +37,7 @@ function SessaoPrincipalHome() {
                         <i className="fa fa-suitcase"></i>
                         <br />
                         <br />
-                        <p>Um simples clique e já está tudo<br/> pronto viajar</p>
+                        <p>Um simples clique e já está tudo<br /> pronto viajar</p>
                     </div>
                     <div className="col-md-4 col-sm-6" id="atendimento">
                         <h4>Fique tranquilo</h4>
@@ -48,7 +45,7 @@ function SessaoPrincipalHome() {
                         <i className="fa fa-headset"></i>
                         <br />
                         <br />
-                        <p>Temos o melhor suporte ao cliente<br/> que você já viu</p>
+                        <p>Temos o melhor suporte ao cliente<br /> que você já viu</p>
                     </div>
                 </div>
             </div>
